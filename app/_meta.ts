c03c25771eac480b51dc24d3e2d085641f8014dd
Ludovic <ludovicdebever0@gmail.com>
2025-02-17 17:37:19 +0100

    feat(docs): add link to app

--- app/_meta.ts
+++ app/_meta.ts
@@ -1,4 +1,9 @@
 export default {
   index: "Castled",
+  github_link: {
+    type: "page",
+    title: "Open Castled",
+    href: "https://castled.app",
+  },
   docs: { title: "Documentation", type: "page" },
 };
